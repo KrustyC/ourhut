e6cbf44d356a8d00ce3e680d23da34fdcf90608c
Davide Crestini <davide.crestini@theculturetrip.com>
2022-02-09 22:29:30 +0000

    feat: almost compelted resources layout

--- src/components/TeacherResourcePanel.tsx
+++ src/components/TeacherResourcePanel.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
 import { parse, format } from "date-fns";
 import { TeachingResource } from "@/types/global";
@@ -16,17 +16,27 @@ export const TeacherResourcePanel: React.FC<TeacherResourcePanelProps> = ({
   teachingResource,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const panelRef = useRef<HTMLDivElement>(null);
 
   const onToggle = () => {
+    if (!isOpen && panelRef.current) {
+      panelRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "start",
+      });
+    }
+
     setIsOpen((currentIsOpen) => !currentIsOpen);
   };
 
   return (
     <div
+      ref={panelRef}
       key={teachingResource._id}
       className="flex flex-col w-full px-12 py-12 mb-8"
     >
-      <div className="flex justify-between items-center mb-8">
+      <div className="flex justify-between items-center">
         <h3 className="text-4xl text-black font-bold">
           {teachingResource.title}
         </h3>
@@ -39,41 +49,45 @@ export const TeacherResourcePanel: React.FC<TeacherResourcePanelProps> = ({
           />
         </span>
       </div>
-      <div className="relative h-60 w-full mb-8">
-        <Image
-          src={teachingResource.image}
-          alt={teachingResource.title}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
 
       {isOpen ? (
-        <table className="w-full">
-          <thead>
-            <tr className="w-1/2 text-left text-sm border-b-2 border-black">
-              <th className="w-1/2 pb-1 px-2">Name of material</th>
-              <th className="w-1/6 pb-1 px-2">Date Published</th>
-              <th className="w-1/4 pb-1 px-2">Host</th>
-              <th className="w-1/12 pb-1 px-2">Link</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {teachingResource.materials.map((material, i) => (
-              <tr key={i} className="w-1/2 py-20 border-b-2 border-black">
-                <td className="py-4 truncate text-ellipsis font-semibold px-2">
-                  {material.name}
-                </td>
-                <td className="py-4 px-2">{formatDate(material.date)}</td>
-                <td className="py-4 truncate text-ellipsis px-2">
-                  {material.host}
-                </td>
-                <td className="py-4 px-2">1961</td>
+        <>
+          <div className="relative h-60 w-full mt-8 mb-8">
+            <Image
+              src={teachingResource.image}
+              alt={teachingResource.title}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <table className="w-full table-fixed">
+            <thead>
+              <tr className="w-1/2 text-left text-sm border-b-2 border-black">
+                <th className="w-1/2 pb-1 px-2">Name of material</th>
+                <th className="w-1/6 pb-1 px-2">Date Published</th>
+                <th className="w-1/4 pb-1 px-2">Host</th>
+                <th className="w-1/12 pb-1 px-2">Link</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {teachingResource.materials.map((material, i) => (
+                <tr key={i} className="py-20 border-b-2 border-black">
+                  <td className="w-1/2  py-4 truncate text-ellipsis font-semibold px-2">
+                    {material.name}
+                  </td>
+                  <td className="w-1/6 py-4 px-2">
+                    {formatDate(material.date)}
+                  </td>
+                  <td className="w-1/4 py-4 overflow-hidden truncate text-ellipsis px-2">
+                    {material.host}
+                  </td>
+                  <td className="w-1/12 py-4 px-2">1961</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       ) : null}
     </div>
   );
--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
 import { TeachingResource } from "@/types/global";
-import { UpArrowIcon } from "@/components/icons/UpArrow";
 import { TeacherResourcePanel } from "@/components/TeacherResourcePanel";
 import { NextPageWithLayout } from "@/types/app";
 import { ResourcesLayout } from "@/layouts/ResourcesLayout";
@@ -14,10 +11,6 @@ interface TeacherResourcesPageProps {
 const TeacherResourcesPage: NextPageWithLayout<TeacherResourcesPageProps> = ({
   teachingResources,
 }) => {
-  const onScrollToTop = () => {
-    window.scroll(0, 0);
-  };
-
   return (
     <>
       <Head>
@@ -38,54 +31,6 @@ const TeacherResourcesPage: NextPageWithLayout<TeacherResourcesPageProps> = ({
       </div>
     </>
   );
-
-  // return (
-  //   <div>
-  //     <Head>
-  //       <title>Resources | Our Hut</title>
-  //       <meta
-  //         name="description"
-  //         content="A comprehensive list of all our resources"
-  //       />
-  //     </Head>
-
-  //     <div className="bg-white pb-8">
-  //       <Navbar
-  //         config={{
-  //           burgerColor: "bg-primary",
-  //           textColor: "text-black",
-  //           logoColor: "bg-primary",
-  //         }}
-  //       />
-
-  //       <div className="px-32">
-  //         <h1 className="text-6xl text-black font-bold my-8">Resources</h1>
-
-  //         <div id="teacher-resources-list" className="flex flex-col">
-  //           {teachingResources.map((teachingResource, i) => (
-  //             <TeacherResourcePanel
-  //               key={teachingResource._id}
-  //               teachingResource={teachingResource}
-  //             />
-  //           ))}
-  //         </div>
-
-  //         <div className="w-full flex justify-end">
-  //           <span
-  //             role="button"
-  //             className="text-xl flex items-center"
-  //             onClick={onScrollToTop}
-  //           >
-  //             Back to top{" "}
-  //             <UpArrowIcon className="ml-2 h-6 w-6 cursor-pointer" />
-  //           </span>
-  //         </div>
-  //       </div>
-  //     </div>
-
-  //     <Footer />
-  //   </div>
-  // );
 };
 
 export async function getStaticProps() {
@@ -97,12 +42,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      teachingResources: [
-        ...teachingResources,
-        ...teachingResources,
-        ...teachingResources,
-        ...teachingResources,
-      ],
+      teachingResources,
     },
   };
 }
