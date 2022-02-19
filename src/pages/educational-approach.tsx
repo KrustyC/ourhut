7033f1af3bfc7af416e1da20f80c65413a0a1be6
Davide Crestini <davide.crestini@theculturetrip.com>
2022-02-19 14:26:42 +0000

    feat: added schools movile view

--- src/pages/educational-approach.tsx
+++ src/pages/educational-approach.tsx
@@ -34,19 +34,20 @@ const EducationalApproachPage: NextPage<EducationalApproachPageProps> = ({
           }}
         />
 
-        <div className="pl-32 pr-16">
-          <p className="text-6xl text-white font-bold ml-[-4px]">
+        <div className="px-8 md:pl-32 md:pr-16">
+          <p className="text-2xl md:text-6xl text-white font-bold md:ml-[-4px]">
             Educational Approach
           </p>
-          <div className="flex mt-8">
-            <p className="text-white font-medium w-1/2 pr-40">
+
+          <div className="flex flex-col md:flex-row py-8">
+            <p className="text-white font-medium w-full md:w-1/2 md:pr-40">
               Laoreet viverra venenatis interdum nunc a. Pulvinar metus sagittis
               lorem odio non lorem odio non pharetra massa nullam id. Interdum
               vel, elementum ultrices id. Ornare tempus lorem odio non non enim
               nunc est, facilisis.
             </p>
 
-            <p className="text-white font-medium w-1/2 pr-40">
+            <p className="text-white font-medium w-full mt-8 md:mt-0 md:w-1/2 md:pr-40">
               Laoreet viverra venenatis interdum nunc a. Pulvinar metus sagittis
               lorem odio non lorem odio non pharetra massa nullam id. Interdum
               vel, elementum ultrices id. Ornare tempus lorem odio non non enim
@@ -56,39 +57,48 @@ const EducationalApproachPage: NextPage<EducationalApproachPageProps> = ({
         </div>
       </div>
 
-      <div id="educational-approach-parallax" className="h-[500px] w-screen" />
+      <div
+        id="educational-approach-parallax"
+        className="h-[300px] md:h-[500px] w-screen"
+      />
 
-      <div className="flex flex-col px-32 pt-16 bg-light-gray">
-        <h1 className="text-black font-bold mb-8">
-          Schools we have worked with
-        </h1>
+      <div className="flex flex-col bg-light-gray">
+        <div className="flex flex-col p-8 md:px-32">
+          <h1 className="text-2xl md:text-6xl text-black font-bold md:mb-4 md:mb-8">
+            Schools we have worked with
+          </h1>
 
-        <div className="flex flex-col mt-16 px-16">
-          <div className="flex flex-col">
-            <h3 className="font-bold text-black mb-8">Primary Schools</h3>
-            <div className="grid grid-cols-2 gap-16">
-              {primarySchools.map((school, i) => (
-                <div key={i} className="flex font-semibold">
-                  <span className="text-black mr-2">{school.name},</span>
-                  <span className="text-gray-500 ">
-                    {school.geographicalArea}, {school.postcode}
-                  </span>
-                </div>
-              ))}
+          <div className="flex flex-col mt-8 md:mt-16 md:px-16">
+            <div className="flex flex-col">
+              <h3 className="font-bold text-black mb-4  md:mb-8">
+                Primary Schools
+              </h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                {primarySchools.map((school, i) => (
+                  <div key={i} className="flex font-semibold">
+                    <span className="text-black mr-2">{school.name},</span>
+                    <span className="text-gray-500 ">
+                      {school.geographicalArea}, {school.postcode}
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
 
-          <div className="flex flex-col mt-16 mb-16">
-            <h3 className="font-bold text-black mb-8">Secondary Schools</h3>
-            <div className="grid grid-cols-2 gap-x-16 gap-y-2">
-              {secondarySchools.map((school, i) => (
-                <div key={i} className="flex font-semibold">
-                  <span className="text-black mr-2">{school.name},</span>
-                  <span className="text-gray-500 ">
-                    {school.geographicalArea}, {school.postcode}
-                  </span>
-                </div>
-              ))}
+            <div className="flex flex-col mt-12">
+              <h3 className="font-bold text-black mb-4 md:mb-8">
+                Secondary Schools
+              </h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                {secondarySchools.map((school, i) => (
+                  <div key={i} className="flex flex-wrap font-semibold">
+                    <span className="text-black mr-2">{school.name},</span>
+                    <span className="text-gray-500 ">
+                      {school.geographicalArea}, {school.postcode}
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
